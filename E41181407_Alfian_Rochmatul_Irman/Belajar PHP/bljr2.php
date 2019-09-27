86b291fb47fb73a6c668275192a0aa9441ef4559
alfiannsx98 <alfianrochmatul77@gmail.com>
2019-09-27 07:18:26 +0700

    Belajar membuat class + Instansiasi bbrp class

---
+++ E41181407_Alfian_Rochmatul_Irman/Belajar PHP/bljr2.php
@@ -0,0 +1,9 @@
+<?php
+class Coba
+{ }
+
+$a = new Coba();
+$b = new Coba();
+
+var_dump($a);
+var_dump($b);
---
+++ E41181407_Alfian_Rochmatul_Irman/Belajar PHP/produk.php
@@ -0,0 +1,11 @@
+<?php
+
+//belajar instansiasi
+//jual produk
+//jual komik
+//jual game
+class Produk
+{ }
+
+$produk1 = new Produk();
+$produk2 = new Produk();
