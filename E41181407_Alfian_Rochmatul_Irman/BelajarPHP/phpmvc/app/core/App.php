e3861e4d35d2f0d9138fe50d0546e4707e43bb21
alfiannsx98 <alfianrochmatul77@gmail.com>
2019-09-28 20:10:45 +0700

    benahin error, tp htaccess masi error

--- E41181407_Alfian_Rochmatul_Irman/BelajarPHP/phpmvc/app/core/App.php
+++ E41181407_Alfian_Rochmatul_Irman/BelajarPHP/phpmvc/app/core/App.php
@@ -13,7 +13,7 @@ class App
         if (isset($_GET['url'])) {
             $url = rtrim($_GET['url'], '/');
             $url = filter_var($url, FILTER_SANITIZE_URL);
-            $url = explode('/', $url;
+            $url = explode('/', $url);
             return $url;
         }
     }
