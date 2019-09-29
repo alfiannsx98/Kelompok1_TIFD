3be7255c057707cae3c6732d9464de671ebf7d56
alfiannsx98 <alfianrochmatul77@gmail.com>
2019-09-30 06:35:30 +0700

    pertemuan6

---
+++ E41181407_Alfian_Rochmatul_Irman/BelajarPHP/PHP_dasar/pertemuan6/latihan1.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+        .kotak {
+            width: 30px;
+            height: 30px;
+            background-color: #BADA55;
+            text-align: center;
+            line-height: 30px;
+            margin: 3px;
+            float: left;
+        }
+
+        .kotak:hover {
+            transform: rotate(180deg);
+        }
+    </style>
+</head>
+
+<body>
+    <div class="kotak">aa</div>
+</body>
+
+</html>
