585686da468c3ab5aee0565dfde857e36c97505f
alfiannsx98 <alfianrochmatul77@gmail.com>
2019-09-30 06:31:18 +0700

    belajar pertemuan5

---
+++ E41181407_Alfian_Rochmatul_Irman/BelajarPHP/PHP_dasar/pertemuan5/latihan1.php
@@ -0,0 +1,23 @@
+<?php
+// MEMBUAT ARRAY
+// ELEMEN PADA ARRAY BOLEH MEMILIKI TIPE DATA YANG BEDA
+
+$hari = array("SENIN", "SELASA", "BARU");
+
+// cara baru
+
+$bulan = ["januari", "februari", "maret"];
+
+$arr1 = [123, "tulis", false];
+
+// var_dump bisa ngecek isi suatu variabel atau print_r()
+// menampilkan array
+var_dump($hari);
+echo "<br>";
+print_r($bulan);
+
+echo "<br>";
+
+echo $hari[0];
+
+echo "<br>";
