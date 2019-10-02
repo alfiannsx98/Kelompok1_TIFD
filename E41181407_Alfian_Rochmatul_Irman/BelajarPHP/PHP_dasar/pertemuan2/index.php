a8d8b8db0174c919560e45b18204fbadf2b0335b
WildanE41181398 <wildanmangli29@gmail.com>
2019-10-02 14:31:25 +0700

    latihan3 html

--- E41181407_Alfian_Rochmatul_Irman/BelajarPHP/PHP_dasar/pertemuan2/index.php
+++ E41181407_Alfian_Rochmatul_Irman/BelajarPHP/PHP_dasar/pertemuan2/index.php
@@ -70,7 +70,7 @@ echo $nama_depan . " " . $nama_belakang;
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
+    <meta charset="UTF-8">  
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
