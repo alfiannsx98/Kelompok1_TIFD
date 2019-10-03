9bfa4bab616538806c71b5bdf2b70f5a674ff20d
Irmaaaaan <alfianrochmatul77@gmail.com>
2019-10-03 16:17:30 +0700

    pertemuan 17

---
+++ E41181407_Alfian_Rochmatul_Irman/BelajarPHP/PHP_dasar/pertemuan17/login.php
@@ -0,0 +1,73 @@
+<?php
+session_start();
+
+if (isset($_SESSION["login"])) {
+    header("Location: index.php");
+    exit;
+}
+
+require 'functions.php';
+if (isset($_POST["login"])) {
+
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    // cek username dan password
+    $result = mysqli_query($koneksi, "SELECT * FROM user WHERE username = '$username'");
+
+    if (mysqli_num_rows($result) === 1) {
+        // ngecek apakah ada data ?  jika ada maka akan lanjut kebawah
+        // cek password
+        $row = mysqli_fetch_assoc($result);
+        // $row itu yg ada di db(form nya)
+        if (password_verify($password, $row["password"])) {
+            // jika cocok maka lanjut ke halaman index
+            $_SESSION["login"] = true;
+            // ngecek session untuk login nya
+
+            header("location: index.php");
+            exit;
+        }
+    }
+    $error = true;
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>LOGIN</h1>
+
+    <?php if (isset($error)) : ?>
+        <p style="color: red; font-style=italic;">Username/password salah</p>
+    <?php endif; ?>
+
+    <form action="" method="post">
+        <ul>
+            <li>
+                <label for="username">Username : </label>
+                <input type="text" name="username" id="username">
+            </li>
+            <li>
+                <label for="password">Password : </label>
+                <input type="password" name="password" id="password">
+            </li>
+            <li>
+                <input type="checkbox" name="remember" id="remember">
+                <label for="remember">Remember Me</label>
+            </li>
+            <li>
+                <button type="submit" name="login">Logiin!</button>
+            </li>
+        </ul>
+    </form>
+</body>
+
+</html>
