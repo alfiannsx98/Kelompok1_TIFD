2488accc3f0d6b86feeea314ee8e215766e27435
WildanE41181398 <wildanmangli29@gmail.com>
2019-10-07 02:10:33 +0700

    update php variabel

---
+++ E41181398_Muhammad_Wildan_Aulia_Kahfi/BelajarPHP/PHP_dasar/phpSekolahKoding/learn.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PHP</title>
+</head>
+<body>
+     
+     
+     <?php 
+     // belajar php 
+
+    // variabel pada php
+    $x = "belajar php kuy";
+    $_29 = "php lu";
+    $wil = "eh sorry ya";
+
+     echo "<h1>$x</h1>";
+     echo "Hello $_29<br>";
+     echo "Hai ". $wil ."";
+     echo " $_29";
+     echo "<br>";
+     echo "Ceue";
+     echo "<br>";
+
+     //tipe data string
+     $qq = "qwerty";
+     $ww = "asdfg ";
+
+     echo " $qq" ." atau ". $ww;
+
+
+
+     ?>
+
+     
+</body>
+</html>
