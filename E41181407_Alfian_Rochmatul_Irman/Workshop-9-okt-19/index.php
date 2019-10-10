ff8d6b799362f605639b16c77c574966973d0855
Irmaaaaan <alfianrochmatul77@gmail.com>
2019-10-10 18:49:25 +0700

    tugas p.viktor(belum kelar)

---
+++ E41181407_Alfian_Rochmatul_Irman/Workshop-9-okt-19/index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Tugas Pak Viktor</title>
+</head>
+
+<body>
+    <header>
+        <a href="index.html" id="logo">
+            <h1>
+                Hello World
+            </h1>
+            <h2>
+                lecturer
+            </h2>
+        </a>
+        <nav>
+            <ul>
+                <li><a href="home.html">Home</a></li>
+                <li><a href="about.html">About</a></li>
+                <li><a href="contact.html">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <section>
+        <div class="container">
+            <h1>Panduan Belajar Bahasa Pemrograman</h1>
+            <ul>
+                <li>
+                    <img src="img/java.png" alt="java" class="java">
+                    <p>Bahasa Pemrograman Java</p>
+                </li>
+                <li>
+                    <img src="img/android.png" alt="android" class="android">
+                    <p>Bahasa Pemrograman Android</p>
+                </li>
+                <li>
+                    <img src="img/swift.png" alt="swift" class="swift">
+                    <p>Bahasa Pemrograman Swift</p>
+                </li>
+            </ul>
+        </div>
+    </section>
+
+    <footer>
+        <div class="page">
+            <ul>
+                <li>
+                    <img src="img/fb.png">
+                </li>
+                <li>
+                    <img src="img/twitter.jpg" alt="">
+                </li>
+            </ul>
+        </div>
+        &copy; Copyright 2019 codeMagic.
+    </footer>
+</body>
+
+</html>
