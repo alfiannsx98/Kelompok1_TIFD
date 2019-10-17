eaa75896ec091022e6f9d2d04622c3b995043f2a
Irmaaaaan <alfianrochmatul77@gmail.com>
2019-10-17 13:58:56 +0700

    benahin login

--- sipadi-Project/index.php
+++ sipadi-Project/index.php
@@ -13,7 +13,7 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
     <!-- Custom styles for this template-->
-    <link href="<?= ('assets/'); ?>css/sb-admin-2.min.css" rel="stylesheet">
+    <link href="assets/css/sb-admin-2.min.css" rel="stylesheet">
 </head>
 
 <body class="bg-gradient-primary">
@@ -33,7 +33,7 @@
                                             <input type="text" class="form-control form-control-user" id="email" name="email" placeholder="Masukkan Email Anda" value="">
                                         </div>
                                         <div class="form-group">
-                                            <input type="password" class="form-control form-control-user" id="password" name="password" placehodler="masukkan Password">
+                                            <input type="password" class="form-control form-control-user" id="password" name="password" placeholder="masukkan Password" value="">
                                         </div>
                                         <hr>
                                         <button href="#" class="btn btn-primary btn-user btn-block">Login</button>
@@ -54,10 +54,10 @@
             </div>
         </div>
     </div>
-    <script src="<?= base_url('assets/') ?>vendor/jquery/jquery.min.js"></script>
-    <script src="<?= base_url('assets/') ?>vendor/bootstrap/js/bootstrap.min.js"></script>
-    <script src="<?= base_url('assets/') ?>vendor/jquery-easing/jquery.easing.min.js"></script>
-    <script src="<?= base_url('assets/') ?>js/sb-admin-2.min.js"></script>
+    <script src="assets/vendor/jquery/jquery.min.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="assets/js/sb-admin-2.min.js"></script>
 </body>
 
 </html>
