40cbbf1a20f98d9653e1fabe83d8fd74132f0609
Irmaaaaan <alfianrochmatul77@gmail.com>
2019-11-06 21:05:03 +0700

    lanjutiin

--- sipadi-Project/application/views/admin/index.php
+++ sipadi-Project/application/views/admin/index.php
@@ -269,7 +269,7 @@ require_once 'admin_header.php';
                 <!-- Nav Item - User Information -->
                 <li class="nav-item dropdown no-arrow">
                     <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <span class="mr-2 d-none d-lg-inline text-gray-600 small">irman</span>
+                        <span class="mr-2 d-none d-lg-inline text-gray-600 small">aaaa</span>
                         <img class="img-profile rounded-circle" src="https://source.unsplash.com/QAB-WJcbgJk/60x60">
                     </a>
                     <!-- Dropdown - User Information -->
