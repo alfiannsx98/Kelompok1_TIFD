0072cb52b6b9c548da54d29ec68d6ad9c307b4cf
Irmaaaaan <alfianrochmatul77@gmail.com>
2019-11-07 12:19:32 +0700

    ngedit tampilan sidebar (error tai $_POST nya)

--- sipadi-Project/application/views/admin/index.php
+++ sipadi-Project/application/views/admin/index.php
@@ -1,14 +1,14 @@
 <?php
 require_once 'admin_header.php';
+require '../../../application/controllers/login/functions-login.php';
 session_start();
 
+// $email = $_POST["email_admin"];
+// $result = mysqli_query($koneksi, "SELECT * FROM admin WHERE email_admin = '$email'");
 if (!isset($_SESSION["login"])) {
-    header("Location: ../login/login.php");
+    header("Location: index.php");
     exit;
 }
-$nik = $_COOKIE["id"];
-var_dump($nik);
-require '../../../application/controllers/login/functions-login.php';
 ?>
 <!-- Sidebar -->
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
@@ -22,13 +22,29 @@ require '../../../application/controllers/login/functions-login.php';
     </a>
 
     <!-- Divider -->
-    <hr class="sidebar-divider my-0">
+    <hr class="sidebar-divider">
+    <div class="sidebar-heading">
+        Admin
+    </div>
 
     <!-- Nav Item - Dashboard -->
     <li class="nav-item active">
         <a class="nav-link" href="index.html">
             <i class="fas fa-fw fa-tachometer-alt"></i>
-            <span>Dashboard</span></a>
+            <span>Dashboard</span>
+        </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="index.html">
+            <i class="fas fa-fw fa-users"></i>
+            <span>Data Karyawan</span>
+        </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="index.html">
+            <i class="fas fa-fw fa-store"></i>
+            <span>Data Toko</span>
+        </a>
     </li>
 
     <!-- Divider -->
@@ -36,39 +52,19 @@ require '../../../application/controllers/login/functions-login.php';
 
     <!-- Heading -->
     <div class="sidebar-heading">
-        Interface
+        Barang
     </div>
 
     <!-- Nav Item - Pages Collapse Menu -->
     <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
-            <i class="fas fa-fw fa-cog"></i>
-            <span>Components</span>
+        <a class="nav-link collapsed" href="#">
+            <i class="fas fa-fw fa-box"></i>
+            <span>Data Barang</span>
         </a>
-        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-            <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Custom Components:</h6>
-                <a class="collapse-item" href="buttons.html">Buttons</a>
-                <a class="collapse-item" href="cards.html">Cards</a>
-            </div>
-        </div>
-    </li>
-
-    <!-- Nav Item - Utilities Collapse Menu -->
-    <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
-            <i class="fas fa-fw fa-wrench"></i>
-            <span>Utilities</span>
+        <a class="nav-link collapsed" href="#">
+            <i class="fas fa-fw fa-stream"></i>
+            <span>Data Kategori</span>
         </a>
-        <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
-            <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Custom Utilities:</h6>
-                <a class="collapse-item" href="utilities-color.html">Colors</a>
-                <a class="collapse-item" href="utilities-border.html">Borders</a>
-                <a class="collapse-item" href="utilities-animation.html">Animations</a>
-                <a class="collapse-item" href="utilities-other.html">Other</a>
-            </div>
-        </div>
     </li>
 
     <!-- Divider -->
@@ -76,41 +72,20 @@ require '../../../application/controllers/login/functions-login.php';
 
     <!-- Heading -->
     <div class="sidebar-heading">
-        Addons
+        Transaksi
     </div>
-
-    <!-- Nav Item - Pages Collapse Menu -->
-    <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
-            <i class="fas fa-fw fa-folder"></i>
-            <span>Pages</span>
-        </a>
-        <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
-            <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Login Screens:</h6>
-                <a class="collapse-item" href="login.html">Login</a>
-                <a class="collapse-item" href="register.html">Register</a>
-                <a class="collapse-item" href="forgot-password.html">Forgot Password</a>
-                <div class="collapse-divider"></div>
-                <h6 class="collapse-header">Other Pages:</h6>
-                <a class="collapse-item" href="404.html">404 Page</a>
-                <a class="collapse-item" href="blank.html">Blank Page</a>
-            </div>
-        </div>
-    </li>
 
     <!-- Nav Item - Charts -->
     <li class="nav-item">
         <a class="nav-link" href="charts.html">
-            <i class="fas fa-fw fa-chart-area"></i>
-            <span>Charts</span></a>
+            <i class="fas fa-fw fa-shopping-cart"></i>
+            <span>Transaksi</span></a>
     </li>
-
     <!-- Nav Item - Tables -->
     <li class="nav-item">
         <a class="nav-link" href="tables.html">
-            <i class="fas fa-fw fa-table"></i>
-            <span>Tables</span></a>
+            <i class="fas fa-fw fa-shipping-fast"></i>
+            <span>Kurir</span></a>
     </li>
 
     <!-- Divider -->
@@ -271,7 +246,7 @@ require '../../../application/controllers/login/functions-login.php';
                 <!-- Nav Item - User Information -->
                 <li class="nav-item dropdown no-arrow">
                     <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <span class="mr-2 d-none d-lg-inline text-gray-600 small">irman</span>
+                        <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?= $email; ?></span>
                         <img class="img-profile rounded-circle" src="https://source.unsplash.com/QAB-WJcbgJk/60x60">
                     </a>
                     <!-- Dropdown - User Information -->
