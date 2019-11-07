919ed57e6f69530d75e898896c607e834cc88f8f
iamTeddyHariyanto <teddy.premadi2000@gmail.com>
2019-11-07 15:03:43 +0700

    register form

---
+++ E41181886_Teddy_Hariyanto_Premadi_Prakoso/Workshop-26-sept-19/Belajar/SIPADI/register.php
@@ -0,0 +1,76 @@
+<!DOCTYPE Html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Halaman Registrasi</title>
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+  </head>
+  <body>
+     
+      
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+    <div class="row justify-content-center">
+    <div class="col-md-4 col-md-offset-4 form-login">
+    
+    <?php
+    /* handle error */
+    if (isset($_GET['error'])) : ?>
+        <div class="alert alert-warning alert-dismissible" role="alert">
+          <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+          <strong>Warning!</strong> <?=base64_decode($_GET['error']);?>
+        </div>
+    <?php endif;?>
+
+        <div class="outter-form-login">
+            <div class="logo-login">
+                <em class="glyphicon glyphicon-user"></em>
+            </div>
+            <form action="send_register.php" method="post">
+            <h3 class="text-center title-login">Registrasi</h3>
+                <div class="form-group">
+                    <input type="text" class="form-control" name="nickname" placeholder="Nama">
+                </div>
+
+                <div class="form-group">
+                    <input type="text" class="form-control" name="E-mail" placeholder="E-mail">
+                </div>
+
+                <div class="form-group">
+                    <input type="password" class="form-control" name="password" placeholder="Password">
+                </div>
+
+                <div class="form-group">
+                    <input type="text" class="form-control" name="No.HP" placeholder="NO.HP">
+                </div>
+
+                <div class="form-group">
+                    <input type="text" class="form-control" name="No.HP" placeholder="NO.HP">
+                </div>
+
+                <div class="form-group">
+                    <input type="password" class="form-control" name="repassword" placeholder="Re-Password">
+                </div>
+
+                <input type="submit" class="btn btn-block btn-primary" value="REGISTER" />
+                
+                <div class="text-center forget">
+                    <p>Back To <a href="./login.php">Menu Utama</a></p>
+                </div>
+            </form>
+        </div>
+    </div>
+
+    <script src="bootstrap/js/jquery.min.js"></script>
+    <script src="bootstrap/js/popper.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+  </body>
+</html>
