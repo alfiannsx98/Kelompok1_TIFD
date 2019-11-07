8963c94d2289529a865cf3a7fed3bf961fc37fc4
Irmaaaaan <alfianrochmatul77@gmail.com>
2019-11-07 12:54:31 +0700

    tampilan Barang

---
+++ sipadi-Project/application/views/barang/index.php
@@ -0,0 +1,79 @@
+<?php
+require_once 'header.php';
+require '../../../application/controllers/login/functions-login.php';
+session_start();
+
+// $email = $_POST["email_admin"];
+// $result = mysqli_query($koneksi, "SELECT * FROM admin WHERE email_admin = '$email'");
+if (!isset($_SESSION["login"])) {
+    header("Location: ../login/login.php");
+    exit;
+}
+?>
+<?php require_once 'sidebar.php'; ?>
+
+<!-- Modal -->
+<!-- Logout Modal-->
+
+<div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+                <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">×</span>
+                </button>
+            </div>
+            <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+            <div class="modal-footer">
+                <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                <a class="btn btn-primary" href="../../../application/controllers/login/logout.php">Logout</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- End Modal -->
+
+
+<!-- Content Wrapper -->
+<div id="content-wrapper" class="d-flex flex-column">
+
+    <!-- Main Content -->
+    <div id="content">
+
+        <?php require_once 'topbar.php'; ?>
+
+        <!-- Begin Page Content -->
+        <div class="container-fluid">
+
+            <!-- Page Heading -->
+            <div class="d-sm-flex align-items-center justify-content-between mb-4">
+                <h1 class="h3 mb-0 text-gray-800">Data Barang</h1>
+            </div>
+
+            <!-- Content Row -->
+            <div class="row">
+                <!-- Content Row -->
+
+                <div class="row">
+
+                </div>
+
+                <!-- Content Row -->
+                <div class="row">
+
+                    <!-- Content Column -->
+                    <div class="col-lg-6 mb-4">
+
+                    </div>
+
+                </div>
+
+            </div>
+            <!-- /.container-fluid -->
+
+        </div>
+        <!-- End of Main Content -->
+    </div>
+    <?php require_once 'footer.php'; ?>
