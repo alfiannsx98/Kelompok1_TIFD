258c4cfa2fa8658633e1f58633c1084871821400
Irmaaaaan <alfianrochmatul77@gmail.com>
2019-11-07 12:43:42 +0700

    dah

--- sipadi-Project/application/views/admin/admin_footer.php
+++ sipadi-Project/application/views/admin/admin_footer.php
@@ -1,3 +1,23 @@
+<!-- Footer -->
+<footer class="sticky-footer bg-white">
+    <div class="container my-auto">
+        <div class="copyright text-center my-auto">
+            <span>Copyright &copy; Your Website 2019</span>
+        </div>
+    </div>
+</footer>
+<!-- End of Footer -->
+
+</div>
+<!-- End of Content Wrapper -->
+
+</div>
+<!-- End of Page Wrapper -->
+
+<!-- Scroll to Top Button-->
+<a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+</a>
 <!-- Bootstrap core JavaScript-->
 <script src="../../../assets/vendor/jquery/jquery.min.js"></script>
 <script src="../../../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
