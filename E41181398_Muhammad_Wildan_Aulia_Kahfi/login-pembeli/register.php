daea1786d1079d341080ca2fc0850e4e6f471a4d
WildanE41181398 <wildanmangli29@gmail.com>
2019-11-10 12:12:37 +0700

    up

--- E41181398_Muhammad_Wildan_Aulia_Kahfi/login-pembeli/register.php
+++ E41181398_Muhammad_Wildan_Aulia_Kahfi/login-pembeli/register.php
@@ -22,7 +22,7 @@ if (isset($_POST["register"])) {
     <title>Register Page</title>
 
     <link rel="stylesheet" href="../assets/vendor/fontawesome-free/css/all.min.css" type="text/css">
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <!-- <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet"> -->
 
     <!-- Custom styles for this template-->
     <link href="../assets/css/sb-admin-2.min.css" rel="stylesheet">
