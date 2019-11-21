0e36d97a7267c891d0a864aac76627e279ee01e3
iamTeddyHariyanto <teddy.premadi2000@gmail.com>
2019-11-21 14:15:10 +0700

    hmm

--- sipadi-Project/application/views/sipadi/register.php
+++ sipadi-Project/application/views/sipadi/register.php
@@ -25,10 +25,10 @@ if (isset($_POST["register"])) {
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
     <!-- Custom styles for this template-->
-    <link href="../../../Kelompok1_TIFD/sipadi-Project/assets/css/sb-admin-2.min.css" rel="stylesheet">
+    <link href="/../../Kelompok1_TIFD/sipadi-Project/assets/css/sb-admin-2.min.css" rel="stylesheet">
 </head>
 
-<body class="" background=" d.jpg">
+<body class="" background=" images/d.jpg">
     <div class="container col-md-7">
 
         <div class="card o-hidden border-0 shadow-lg my-5">
