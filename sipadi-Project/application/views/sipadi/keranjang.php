ba7bb3e12a4abcc0e1c067d97baee825d2fa3f35
YogaE41181231 <ichayoga1@gmail.com>
2019-11-26 12:38:23 +0700

    xxx

--- sipadi-Project/application/views/sipadi/keranjang.php
+++ sipadi-Project/application/views/sipadi/keranjang.php
@@ -1,5 +1,5 @@
 <?php
-require 'includes/header_content.php';
+require 'includes/headerdtl.php';
 ?>
 
 <div class="container">
@@ -97,7 +97,12 @@ require 'includes/header_content.php';
     </div>
 </form>
 </div>
-
+<script src="js/jquery-3.2.1.min.js"></script>
+<script src="styles/bootstrap4/popper.js"></script>
+<script src="styles/bootstrap4/bootstrap.min.js"></script>
+<script src="plugins/Isotope/isotope.pkgd.min.js"></script>
+<script src="plugins/OwlCarousel2-2.2.1/owl.carousel.js"></script>
+<script src="plugins/easing/easing.js"></script>
+<script src="js/custom.js"></script>
 <?php
     require 'includes/footer.php';
-?>
