835b6a02a393b81e1bf904d293625557cb4a3ad8
YogaE41181231 <ichayoga1@gmail.com>
2019-11-27 16:28:51 +0700

    lossssssss

--- sipadi-Project/application/views/sipadi/index.php
+++ sipadi-Project/application/views/sipadi/index.php
@@ -622,14 +622,6 @@
 		</div>
 	</div>
 
-	<script src="js/jquery-3.2.1.min.js"></script>
-	<script src="styles/bootstrap4/popper.js"></script>
-	<script src="styles/bootstrap4/bootstrap.min.js"></script>
-	<script src="plugins/Isotope/isotope.pkgd.min.js"></script>
-	<script src="plugins/OwlCarousel2-2.2.1/owl.carousel.js"></script>
-	<script src="plugins/easing/easing.js"></script>
-	<script src="js/custom.js"></script>
-
 	<!-- Footer -->
 
 	<?php
@@ -641,125 +633,118 @@
 
 <!-- MODAL DIALOG LOGIN -->
 <div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-dialog modal-dialog-centered" role="document">
     <div class="modal-content">
-      <div class="modal-header border-bottom-0">
+        <div class="modal-header border-bottom-0">
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
+            <span aria-hidden="true">&times;</span>
         </button>
-      </div>
-      <div class="modal-body">
-        <div class="form-title text-center">
-          <h4>Login</h4>
         </div>
-        <div class="d-flex flex-column text-center">
-         
+        <div class="modal-body">
+        <div class="form-title text-center">
+            <h4>Login</h4>
+        </div>
+        <div class="d-flex flex-column text-center"> 
             <div class="form-group">
-              <input type="email" class="form-control" id="email1"placeholder="Your email address...">
+                <input type="email" class="form-control" id="email1"placeholder="Your email address...">
             </div>
             <div class="form-group">
-              <input type="password" class="form-control" id="password1" placeholder="Your password...">
+                <input type="password" class="form-control" id="password1" placeholder="Your password...">
 			</div>
 			<div class="form-group">
 			<button type="button" class="btn btn-info col-md-4 btn-round">Login</button>
 			<button href="#" type="button" class="btn btn-success btn-info col-md-4 btn-round" data-toggle="modal" data-target="#forgetModal" aria-hidden="true">Lupa Password</button>
 			</div>
-          <div class="d-flex justify-content-center social-buttons">
+            <div class="d-flex justify-content-center social-buttons">
         </div>
-      </div>
+        </div>
     </div>
-  </div>
+    </div>
 </div>
 </div>
 
 <!--lupa passwoerd-->
 
 <div class="modal fade" id="forgetModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-dialog modal-dialog-centered" role="document">
     <div class="modal-content">
-      <div class="modal-header border-bottom-0">
+        <div class="modal-header border-bottom-0">
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
+            <span aria-hidden="true">&times;</span>
         </button>
-      </div>
-      <div class="modal-body">
+        </div>
+        <div class="modal-body">
         <div class="form-title text-center">
-          <h4>Lupa Password</h4>
+            <h4>Lupa Password</h4>
         </div>
         <div class="d-flex flex-column text-center">
-          <form>
+            <form>
             <div class="form-group">
-              <input type="text" class="form-control" id="namaemail"placeholder="Alamat Email">
+                <input type="text" class="form-control" id="namaemail"placeholder="Alamat Email">
 
             <button type="button" class="btn btn-info btn-block btn-round">KIRIM</button>
-
-			<a href="login.php" class="btn btn-success btn-user btn-block">kembali ke Login</a>
-
 			<a href="index.php" class="btn btn-success btn-user btn-block">kembali</a>
-
-			<a href="index.php" class="btn btn-success btn-user btn-block">kembali</a>
->>>>>>> parent of 34bd077... bbbb
-          </form>
-          <div class="d-flex justify-content-center social-buttons">
+            </form>
+            <div class="d-flex justify-content-center social-buttons">
         </div>
-      </div>
+        </div>
     </div>
-  </div>
+    </div>
 </div>
 </div>
 </div>
 <!--cart-->
 
 <div class="modal fade" id="cartModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+    <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
     <div class="modal-content">
-      <div class="modal-header border-bottom-0">
+        <div class="modal-header border-bottom-0">
         <h5 class="modal-title" id="exampleModalLabel">
-          Your Shopping Cart
+            Your Shopping Cart
         </h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
+            <span aria-hidden="true">&times;</span>
         </button>
-      </div>
-      <div class="modal-body">
+        </div>
+        <div class="modal-body">
         <table class="table table-image">
-          <thead>
+            <thead>
             <tr>
-              <th scope="col"></th>
-              <th scope="col">Produk</th>
-              <th scope="col">Harga</th>
-              <th scope="col">Jumlah</th>
-              <th scope="col">Total</th>
-              <th scope="col">Actions</th>
+                <th scope="col"></th>
+                <th scope="col">Produk</th>
+                <th scope="col">Harga</th>
+                <th scope="col">Jumlah</th>
+                <th scope="col">Total</th>
+                <th scope="col">Actions</th>
             </tr>
-          </thead>
-          <tbody>
+            </thead>
+            <tbody>
             <tr>
-              <td class="w-25">
+                <td class="w-25">
                 <img src="https://s3.eu-central-1.amazonaws.com/bootstrapbaymisc/blog/24_days_bootstrap/vans.png" class="img-fluid img-thumbnail" alt="Sheep">
-              </td>
-              <td>Vans Sk8-Hi MTE Shoes</td>
-              <td>89$</td>
-              <td class="qty"><input type="text" class="form-control" id="input1" value="2"></td>
-              <td>178$</td>
-              <td>
+                </td>
+                <td>Vans Sk8-Hi MTE Shoes</td>
+                <td>89$</td>
+                <td class="qty"><input type="text" class="form-control" id="input1" value="2"></td>
+                <td>178$</td>
+                <td>
                 <a href="#" class="btn btn-danger btn-sm">
-                  <i class="fa fa-times"></i>
+                    <i class="fa fa-times"></i>
                 </a>
-              </td>
+                </td>
             </tr>
-          </tbody>
+            </tbody>
         </table> 
         <div class="d-flex justify-content-end">
-          <h5>Total: <span class="price text-success">89$</span></h5>
+            <h5>Total: <span class="price text-success">89$</span></h5>
         </div>
-      </div>
-      <div class="modal-footer border-top-0 d-flex justify-content-between">
+        </div>
+        <div class="modal-footer border-top-0 d-flex justify-content-between">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
         <button type="button" class="btn btn-success">Checkout</button>
-      </div>
+        </div>
     </div>
-  </div>
+    </div>
 </div>
 
 <script src="js/jquery-3.2.1.min.js"></script>
