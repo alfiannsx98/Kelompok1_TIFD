35c2f09956e6545bf4636f1603a7e92ca0c3b66b
YogaE41181231 <ichayoga1@gmail.com>
2019-12-03 09:40:28 +0700

    slider

---
+++ sipadi-Project/application/views/sipadi/includes/slider.php
@@ -0,0 +1,14 @@
+<div class="main_slider" style="background-image:url(images/slide01.jpg)">
+			<div class="container fill_height">
+				<div class="row align-items-center fill_height">
+					<div class="col">
+						<div class="main_slider_content">
+							<h1>SIPADI</h1>
+							<h6>Sistem Informasi Pertanian Sumberdadi</h6>
+								<div class="red_button shop_now_button"><a href="#produk">shop now</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
