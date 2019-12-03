18ac33622ba3d192a6d5a8d05b73d7653136b455
Irmaaaaan <alfianrochmatul77@gmail.com>
2019-12-03 09:37:49 +0700

    test

---
+++ sipadi-Project/application/views/operator/sidebar.php
@@ -0,0 +1,53 @@
+<!-- Sidebar -->
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+    <!-- Sidebar - Brand -->
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
+        <div class="sidebar-brand-icon rotate-n-15">
+            <i class="fas fa-laugh-wink"></i>
+        </div>
+        <div class="sidebar-brand-text mx-3">Operator SiPadi</div>
+    </a>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+    <div class="sidebar-heading">
+        Admin
+    </div>
+
+    <!-- Nav Item - Dashboard -->
+    <li class="nav-item active">
+        <a class="nav-link" href="">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Dashboard</span>
+        </a>
+    </li>
+
+    <!-- Nav Item - Pages Collapse Menu -->
+
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+
+    <!-- Heading -->
+    <div class="sidebar-heading">
+        Transaksi
+    </div>
+
+    <!-- Nav Item - Charts -->
+    <li class="nav-item">
+        <a class="nav-link" href="transaksi/">
+            <i class="fas fa-fw fa-shopping-cart"></i>
+            <span>Transaksi</span></a>
+    </li>
+    <!-- Nav Item - Tables -->
+
+    <!-- Divider -->
+    <hr class="sidebar-divider d-none d-md-block">
+
+    <!-- Sidebar Toggler (Sidebar) -->
+    <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+    </div>
+
+</ul>
+<!-- End of Sidebar -->
---
+++ sipadi-Project/application/views/operator/transaksi/autofill_ongkir.php
@@ -0,0 +1,9 @@
+<?php
+require '../../../controllers/transaksi/functions-transaksi.php';
+
+$id = $_GET['id'];
+
+$sql = mysqli_query($koneksi, "SELECT * FROM kurir WHERE id_kurir='$id'");
+$ongkir = mysqli_fetch_array($sql);
+$data = array('ongkir_kurir' => $ongkir['ongkir_kurir']);
+echo json_encode($data);
