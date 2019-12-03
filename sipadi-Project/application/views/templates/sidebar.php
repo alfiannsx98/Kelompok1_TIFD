6d2b934a3cd44649e644e8c720ed093791d2701a
WildanE41181398 <wildanmangli29@gmail.com>
2019-12-03 15:06:24 +0700

    sek sek urong

--- sipadi-Project/application/views/templates/sidebar.php
+++ sipadi-Project/application/views/templates/sidebar.php
@@ -17,7 +17,7 @@
 
     <!-- Nav Item - Dashboard -->
     <li class="nav-item">
-        <a class="nav-link" href="../admin">
+        <a class="nav-link" href="../dashboard/dashboard.php">
             <i class="fas fa-fw fa-tachometer-alt"></i>
             <span>Dashboard</span>
         </a>
