315636d353e6f2a5e2b0d6ee0b4313c191038813
WildanE41181398 <wildanmangli29@gmail.com>
2019-12-08 16:45:50 +0700

    hadeh

--- sipadi-Project/application/views/dashboard/index.php
+++ sipadi-Project/application/views/dashboard/index.php
@@ -185,7 +185,7 @@
             </div>
           </div>
 
-          <script src="vendor/jquery/jquery.min.js"></script>
+          <script src="../assets/vendor/jquery/jquery.min.js"></script>
   <script src="../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
   <!-- Core plugin JavaScript-->
