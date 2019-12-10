00b0a61de85c5873dcaf2cd3beb9fdf3ede2385a
iamTeddyHariyanto <teddy.premadi2000@gmail.com>
2019-12-10 09:41:26 +0700

    buat profil

---
+++ sipadi-Project/application/views/sipadi/profil.php
@@ -0,0 +1,77 @@
+
+
+<div class="super_container">
+
+	<!-- Header -->
+
+	<?php 
+		require 'includes/headerprofil.php' ;
+	?>
+	<div class="container single_product_container">
+		<div class="row">
+			<div class="col">
+            <div class="kotakk">
+            <center><h2>Profil User</h2></center>
+            </div>
+				<!-- Breadcrumbs -->
+            
+				<div class="breadcrumbs d-flex flex-row align-items-center">
+					<ul>
+						<li><a href="index.php">Home</a></li>
+						<li><a href="#"><i class="fa fa-angle-right" aria-hidden="true"></i>Profil</a></li>
+					</ul>
+				</div>
+
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-lg-7">
+				<div class="single_product_pics">
+					<div class="row">
+						<div class="col-lg-10 image_col order-lg-2  order-1">
+							<div class="single_product_image">
+								<div class="single_product_image_background" style="background-image:url(images/single_1.jpg)"></div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-5">
+				<div class="product_details">
+					<div class="product_details_title">
+                    <div class="kotak">
+						<h3>Nama        : Teddy Hariyanto P.P</h3>
+                        <hr>
+						<h3>Tgl Lahir   : Madura, 03-mei-1999</h3>
+                        <hr>
+                        <h3>No Wa       : 084 987 654 432</h3>
+                        <hr>
+						<h3>Ig          : Teddy_Kmue</h3>
+                        <hr>
+						<p>"Hiduplah seperti LERRY...!!!!"</p>
+                        <p>“Tidak penting seberapa lambat anda melaju, selagi anda tidak berhenti”</p>
+                    </div>
+					</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+	</div>
+
+    <?php 
+        require 'includes/footer.php' ;
+    ?>
+
+<script src="js/jquery-3.2.1.min.js"></script>
+<script src="styles/bootstrap4/popper.js"></script>
+<script src="styles/bootstrap4/bootstrap.min.js"></script>
+<script src="plugins/Isotope/isotope.pkgd.min.js"></script>
+<script src="plugins/OwlCarousel2-2.2.1/owl.carousel.js"></script>
+<script src="plugins/easing/easing.js"></script>
+<script src="plugins/jquery-ui-1.12.1.custom/jquery-ui.js"></script>
+<script src="js/single_custom.js"></script>
+</body>
+
+</html>
