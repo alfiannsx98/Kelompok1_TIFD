1acca1316f2e75a8dca2d947370ee8fef7e9be31
WildanE41181398 <wildanmangli29@gmail.com>
2019-12-10 10:20:18 +0700

    ok su

--- sipadi-Project/application/views/admin/index.php
+++ sipadi-Project/application/views/admin/index.php
@@ -65,7 +65,46 @@ $gmbr = mysqli_fetch_assoc($sql);
             <!-- Content Row -->
             <div class="row">
                 <!-- Content Row -->
+                <div class="col-xl-3 col-md-6 mb-4">
+              <div class="card border-left-primary shadow h-100 py-2">
+                <div class="card-body">
+                  <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                      <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Data Total Penjualan</div>
+                      <?php $dt = query("SELECT SUM(total_final) AS duit FROM transaksi"); ?>
+                      <?php foreach($dt as $duit): ?>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800">Rp. <?=  $duit['duit'];?></div>
+                      <?php endforeach; ?>
+                    </div>
+                    <div class="col-auto">
+                      <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
 
+            <div class="col-xl-3 col-md-6 mb-4">
+              <div class="card border-left-success shadow h-100 py-2">
+                <div class="card-body">
+                  <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                      <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Jumlah User Admin Terdaftar</div>
+                      <?php 
+                      $dtusr = mysqli_query($koneksi,"SELECT * FROM admin");
+                      $usr = mysqli_num_rows($dtusr); 
+                      ?>
+                      
+                      <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $usr; ?></div>
+                    </div>
+                    <div class="col-auto">
+                      <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            
                 <div class="row">
 
                 </div>
