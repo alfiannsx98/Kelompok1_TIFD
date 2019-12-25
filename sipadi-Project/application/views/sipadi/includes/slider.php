55b89b7bde9d6c71c0d4709f68c7aca015696572
YogaE41181231 <ichayoga1@gmail.com>
2019-12-25 23:04:23 +0700

    coursel

--- sipadi-Project/application/views/sipadi/includes/slider.php
+++ sipadi-Project/application/views/sipadi/includes/slider.php
@@ -1,14 +1,35 @@
-<div class="main_slider" style="background-image:url(images/slide01.jpg)">
-			<div class="container fill_height">
-				<div class="row align-items-center fill_height">
-					<div class="col">
-						<div class="main_slider_content">
-							<h1>SIPADI</h1>
-							<h6>Sistem Informasi Pertanian Sumberdadi</h6>
-								<div class="red_button shop_now_button"><a href="#produk">shop now</a>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+<div id="demo" class="carousel slide" data-ride="carousel" style="margin-top:40px">
+
+  <!-- Indicators -->
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+
+  <!-- The slideshow -->
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="images/slide01.jpg" alt="slide01" width="100%">
+	  <div class="carousel-caption">
+    	<h1><span>SIPADI</span></h1>
+    	<p><span>Sistem Informasi Pertanian Sumberdadi</span></p>
+  	  </div>
+    </div>
+    <div class="carousel-item">
+      <img src="images/slide02.jpg" alt="slide02" width="100%">
+    </div>
+    <div class="carousel-item">
+      <img src="images/slide03.jpg" alt="slide03" width="100%">
+    </div>
+  </div>
+
+  <!-- Left and right controls -->
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+
+</div>
