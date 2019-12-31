cd9d28a81ed02bac7987e744dcb8cdc5d109fedd
Irmaaaaan <alfianrochmatul77@gmail.com>
2019-12-31 13:36:21 +0700

    sudah bisa ganti password user

--- sipadi-Project/application/controllers/login/PHPMailer/config-user.php
+++ sipadi-Project/application/controllers/login/PHPMailer/config-user.php
@@ -26,7 +26,7 @@ try {
     $mail->isSMTP();                                            // Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'alfianrochmatul77@gmail.com';                     // SMTP username
+    $mail->Username   = 'asdasd@gmail.com';                     // SMTP username
     $mail->Password   = 'indowebster9';                               // SMTP password
     $mail->SMTPSecure = 'tls';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
     $mail->Port       = 587;                                   // TCP port to connect to
