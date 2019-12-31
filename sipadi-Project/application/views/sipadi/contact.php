3d4a600f4e5c98a8bf599ad0eee8019e07cbd27f
WildanE41181398 <wildanmangli29@gmail.com>
2019-12-31 18:48:05 +0700

    hewaunja

--- sipadi-Project/application/views/sipadi/contact.php
+++ sipadi-Project/application/views/sipadi/contact.php
@@ -91,13 +91,13 @@
 						<h1>Hubungi Kami!</h1>
 						<form action="post">
 							<div>
-								<input id="input_name" class="form_input input_name input_ph" type="text" name="name" placeholder="Name" required="required" data-error="Name is required.">
+								<input id="input_name" class="form_input input_name input_ph" type="text" name="name" placeholder="Nama" required="required" data-error="Name is required.">
 								<input id="input_email" class="form_input input_email input_ph" type="email" name="email" placeholder="Email" required="required" data-error="Valid email is required.">
-								<input id="input_website" class="form_input input_website input_ph" type="url" name="name" placeholder="Website" required="required" data-error="Name is required.">
-								<textarea id="input_message" class="input_ph input_message" name="message" placeholder="Message" rows="3" required data-error="Please, write us a message."></textarea>
+								<input id="input_nohp" class="form_input input_nohp input_ph" type="text" name="name" placeholder="NO HP" required="required" data-error="Nomor is required.">
+								<textarea id="input_message" class="input_ph input_message" name="message" placeholder="Pesan" rows="3" required data-error="Please, write us a message."></textarea>
 							</div>
 							<div>
-								<button id="review_submit" type="submit" class="red_button message_submit_btn trans_300" value="Submit">send message</button>
+								<button id="review_submit" type="submit" class="red_button message_submit_btn trans_300" value="Submit">Kirim Pesan</button>
 							</div>
 						</form>
 					</div>
