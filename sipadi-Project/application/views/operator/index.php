4e083065d856d2ee4c25d51b204ce1a9b2127dc4
iamTeddyHariyanto <teddy.premadi2000@gmail.com>
2019-12-31 13:09:08 +0700

    push dulu

--- sipadi-Project/application/views/operator/index.php
+++ sipadi-Project/application/views/operator/index.php
@@ -62,8 +62,179 @@ $gmbr = mysqli_fetch_assoc($sql);
                 <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
             </div>
 
-            <!-- Content Row -->
-            <div class="row">
+<!--batas aku copas dari admin-->
+            <?php
+            $var = "Belum Terkonfirmasi";
+            $krjg = mysqli_query($koneksi, "SELECT * FROM transaksi WHERE  `transaksi`.`id_adm` = '$var' OR `transaksi`.`status_kirim` = 0");
+            $cart = mysqli_num_rows($krjg);
+            ?>
+
+<?php if ($cart >= 1) : ?>
+        <div class="alert alert-warning" role="alert">
+          Haloo <?= $email; ?> Ada <span class="badge badge-pill badge-success"><?= $cart; ?></span> pesanan yang belum dikonfirmasi nih!
+        </div>
+      <?php endif; ?>
+      <!-- Content Row -->
+      <div class="row">
+        <!-- Content Row -->
+        <div class="col-xl-3 col-md-6 mb-4">
+          <div class="card border-left-primary shadow h-100 py-2">
+            <div class="card-body">
+              <div class="row no-gutters align-items-center">
+                <div class="col mr-2">
+                  <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Data Total Penjualan</div>
+                  <?php $dt = query("SELECT SUM(total_final) AS duit FROM transaksi"); ?>
+                  <?php foreach ($dt as $duit) : ?>
+                    <div class="h5 mb-0 font-weight-bold text-gray-800">Rp. <?= number_format($duit['duit']);  ?></div>
+                  <?php endforeach; ?>
+                </div>
+                <div class="col-auto">
+                  <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-xl-3 col-md-6 mb-4">
+          <div class="card border-left-success shadow h-100 py-2">
+            <div class="card-body">
+              <div class="row no-gutters align-items-center">
+                <div class="col mr-2">
+                  <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Jumlah Operator yang Terdaftar</div>
+                  <?php
+                  $var1 = "2";
+                  $dtusr = mysqli_query($koneksi, "SELECT * FROM admin where level='$var1'");
+                  $usr = mysqli_num_rows($dtusr);
+                  ?>
+                  <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $usr; ?></div>
+                </div>
+                <div class="col-auto">
+                  <i class="fas fa-user-lock"></i>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-xl-3 col-md-6 mb-4">
+          <div class="card border-left-info shadow h-100 py-2">
+            <div class="card-body">
+              <div class="row no-gutters align-items-center">
+                <div class="col mr-2">
+                  <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Jumlah Pembeli yang Terdaftar</div>
+                  <?php
+                  $dtkry = mysqli_query($koneksi, "SELECT * FROM pembeli");
+                  $kry = mysqli_num_rows($dtkry);
+                  ?>
+                  <div class="row no-gutters align-items-center">
+                    <div class="col-auto">
+                      <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800"><?= $kry; ?></div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-auto">
+                  <i class="fas fa-users"></i>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <?php
+        $var = "1";
+        $krjg = mysqli_query($koneksi, "SELECT * FROM transaksi WHERE  `transaksi`.`id_adm` = '$var' OR `transaksi`.`status_kirim` = 1");
+        $cart = mysqli_num_rows($krjg);
+        ?>
+        <div class="col-xl-3 col-md-6 mb-4">
+          <div class="card border-left-warning shadow h-100 py-2">
+            <div class="card-body">
+              <div class="row no-gutters align-items-center">
+                <div class="col mr-2">
+                  <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Transaksi Berhasil Terkonfirmasi</div>
+                  <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $cart; ?></div>
+                </div>
+                <div class="col-auto">
+                  <i class="fas fa-shopping-cart"></i>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+
+        <!-- Area Chart -->
+        <div class="col-xl-8 col-lg-7">
+          <div class="card shadow mb-4">
+            <!-- Card Header - Dropdown -->
+            <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+              <h6 class="m-0 font-weight-bold text-primary">Earnings Overview</h6>
+              <div class="dropdown no-arrow">
+                <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
+                </a>
+                <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink">
+                  <div class="dropdown-header">Dropdown Header:</div>
+                  <a class="dropdown-item" href="#">Action</a>
+                  <a class="dropdown-item" href="#">Another action</a>
+                  <div class="dropdown-divider"></div>
+                  <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+              </div>
+            </div>
+
+            <!-- Card Body -->
+            <div class="card-body">
+              <div class="chart-area">
+                <canvas id="ChartKu"></canvas>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- Pie Chart -->
+        <div class="col-xl-4 col-lg-5">
+          <div class="card shadow mb-4">
+            <!-- Card Header - Dropdown -->
+            <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+              <h6 class="m-0 font-weight-bold text-primary">Revenue Sources</h6>
+              <div class="dropdown no-arrow">
+                <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
+                </a>
+                <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink">
+                  <div class="dropdown-header">Dropdown Header:</div>
+                  <a class="dropdown-item" href="#">Action</a>
+                  <a class="dropdown-item" href="#">Another action</a>
+                  <div class="dropdown-divider"></div>
+                  <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+              </div>
+            </div>
+            <!-- Card Body -->
+            <div class="card-body">
+              <div class="chart-pie pt-4 pb-2">
+                <canvas id="myPieChart"></canvas>
+              </div>
+              <div class="mt-4 text-center small">
+                <span class="mr-2">
+                  <i class="fas fa-circle text-primary"></i> Direct
+                </span>
+                <span class="mr-2">
+                  <i class="fas fa-circle text-success"></i> Social
+                </span>
+                <span class="mr-2">
+                  <i class="fas fa-circle text-info"></i> Referral
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+<!--batas akhir copas dari index.php admin-->
                 <!-- Content Row -->
 
                 <div class="row">
